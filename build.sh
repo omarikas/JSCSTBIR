950e16a1420678a246051e462b5c65450e1105a0
omarikas <86628361+omarikas@users.noreply.github.com>
2024-05-15 18:26:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
 curl -L -o ./model.tar.gz\
   https://www.kaggle.com/api/v1/models/omarbazid/jscstbir/tfJs/v-1/1/download
-tar -xzf model.tar.gz
+tar -xzf ./model.tar.gz -C /opt/render/project/src/model
 
 npm install --force
